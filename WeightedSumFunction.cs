02433df00bd0895013fccc801ad536bbb538bc2e
RLKane47 <39221706+RLKane47@users.noreply.github.com>
2018-05-30 11:27:10 -0500

    Create WeightedSumFunction.cs

---
+++ WeightedSumFunction.cs
@@ -0,0 +1,7 @@
+public class WeightedSumFunction : IInputFunction
+{
+    public double CalculateInput(List<ISynapse> inputs)
+    {
+        return inputs.Select(x => x.Weight * x.GetOutput()).Sum();
+    }
+}
